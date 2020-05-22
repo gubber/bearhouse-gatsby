12c23f65f8ae192b9f0d5eeaeeb3e84649c64a27
David Butt <dave@dgbweb.co.uk>
2020-05-23 00:32:53 +0100

    Update thanks message

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,8 +6,8 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Thank you, your message has been sent.</h1>
+          <p>We will be in touch soon.</p>
         </div>
       </div>
     </section>
